127ef0707145e497f5cdae8ae36d533c23f6d27e
Ali Alhakeem <alismk@MBA.local>
2026-02-05 10:50:34 +0000

    change

--- A1.cpp
+++ A1.cpp
@@ -3,11 +3,11 @@ int quantum_num_i; int quantum_num_j;
 const double Rydberg{13.6};
 int atomic_num;
  int main() {
-    std::cout<<" please enter the atomic number";
+    std::cout<<" please enter the atomic number f: ";
     std::cin>>atomic_num;
-    std::cout<<" please enter the quantum number i";
+    std::cout<<" please enter the quantum number i: ";
     std::cin>>quantum_num_i;
-    std::cout<<" please enter the quantum number j";
+    std::cout<<" please enter the quantum number j: ";
     std::cin>>quantum_num_j;
     
     double energy = Rydberg*atomic_num*atomic_num*(1.0/(quantum_num_j*quantum_num_j)-1.0/(quantum_num_i*quantum_num_i));
